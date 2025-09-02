f857f24db3447cb4fc34c1529684ec6280d634d3
Nroseman <nicholas.roseman@gmail.com>
2025-09-01 22:07:14 -0700

    reworked main and helper funcs

--- blackjack/helpers.py
+++ blackjack/helpers.py
@@ -1,1 +1,46 @@
-# wip
+import random
+
+
+def create_shoe(num_decks):
+    cards = [
+        {"face": "2", "value": 2},
+        {"face": "3", "value": 3},
+        {"face": "4", "value": 4},
+        {"face": "5", "value": 5},
+        {"face": "6", "value": 6},
+        {"face": "7", "value": 7},
+        {"face": "8", "value": 8},
+        {"face": "9", "value": 9},
+        {"face": "10", "value": 10},
+        {"face": "J", "value": 10},
+        {"face": "Q", "value": 10},
+        {"face": "K", "value": 10},
+        {"face": "A", "value": 11}
+    ]
+
+    shoe = [(card["face"], card["value"]) for card in cards] * 4 * num_decks
+
+    random.shuffle(shoe)
+
+    return shoe
+
+
+def create_spots(NUM_PLAYERS, CHIPS):
+    spots = [{"index": i, "chips": CHIPS, "hands": []}
+             for i in range(NUM_PLAYERS + 1)]
+    return spots
+    # dealer = spots[0]
+    # players = spots[1:]
+    # return dealer, players
+
+
+def deal_card(shoe, cards, num_cards):
+    dealt = shoe[:num_cards]
+    cards.extend(dealt)
+    del shoe[:num_cards]
+    return True
+
+
+def reset(spots):
+    for player in spots:
+        player["hands"] = []
--- blackjack/main.py
+++ blackjack/main.py
@@ -1,56 +1,84 @@
-# TODO break into functions:
-#       - dealing
-#       - evaluating
-
-from create_shoe import create_shoe
+from helpers import create_shoe, create_spots, reset, deal_card
 import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-num_decks = 2
-players = 2
+NUM_DECKS = 2
+NUM_PLAYERS = 2
+STARTING_CHIPS = 1000
 
 
 def main():
 
-    shoe = create_shoe(num_decks)
-    shoe_length = len(shoe)
-    shoe_penetration = int(shoe_length * .75)
-    discard = []
-    next_action = None
-    spots = [[] for spot in range(players + 1)]  # TODO make dict instead?
-    bj = False
-    soft = False
+    # CREATE TABLE
+    # NEW SHOE OF CARDS
+    shoe = create_shoe(NUM_DECKS)
+    # SETUP PLAYERS {INDEX, CHIPS, HANDS: [CARDS, TOTAL, SOFT, BJ]}?
+    spots = create_spots(NUM_PLAYERS, STARTING_CHIPS)
 
-    while len(discard) < shoe_penetration:
-        # deal two cards to players and dealer
-        for x, spot in enumerate(spots):
-            bj = False
-            soft = False
-            spot = shoe[:2]
-            discard.extend(spot)
-            del shoe[:2]
+    # START ROUND
+    # INITIAL DEAL
+    for spot in spots:
+        deal_card(shoe, spot["hands"], 2)
+    dealer = spots[0]
+    players = spots[1:]
+    # EVALUATE
 
-            if x < players:
-                total = spot[0][1] + spot[1][1]
-                if total == 21:
-                    bj = True
-                elif "A" in (spot[0][0], spot[1][0]):
-                    if total > 21:
-                        total -= 10
-                    else:
-                        soft = True
+    # END OF ROUND
+    # RESET
+    # reset(spots)
 
-                print(f"Player {x+1}: {spot[0][0]} {spot[1][0]} ", end="")
-                if bj:
-                    print("blackjack!")
-                elif soft:
-                    print(f"(soft {total})")
-                else:
-                    print(f"({total})")
+    #
 
-            else:
-                print(f"\nDealer is showing a {spot[0][0]}\n")
+    # CHOICES
+    # HIT STAND DOUBLE SPLIT
+    # DEAL CARD IF APPLICABLE
+
+    # EVALUATE
+    # CHECK TOTAL
+    # CHECK SOFT
+    # CHECK FOR BJ
+
+    # RESOLVE
+
+    # shoe = create_shoe(num_decks)
+    # shoe_length = len(shoe)
+    # shoe_penetration = int(shoe_length * .75)
+    # discard = []
+    # next_action = None
+    # spots = [[] for spot in range(players + 1)]  # TODO make dict instead?
+    # bj = False
+    # soft = False
+
+    # while len(discard) < shoe_penetration:
+    #     # deal two cards to players and dealer
+    #     for x, spot in enumerate(spots):
+    #         bj = False
+    #         soft = False
+    #         spot = shoe[:2]
+    #         discard.extend(spot)
+    #         del shoe[:2]
+
+    #         if x < players:
+    #             total = spot[0][1] + spot[1][1]
+    #             if total == 21:
+    #                 bj = True
+    #             elif "A" in (spot[0][0], spot[1][0]):
+    #                 if total > 21:
+    #                     total -= 10
+    #                 else:
+    #                     soft = True
+
+    #             print(f"Player {x+1}: {spot[0][0]} {spot[1][0]} ", end="")
+    #             if bj:
+    #                 print("blackjack!")
+    #             elif soft:
+    #                 print(f"(soft {total})")
+    #             else:
+    #                 print(f"({total})")
+
+    #         else:
+    #             print(f"\nDealer is showing a {spot[0][0]}\n")
 
     # TODO Add player options
     # for player in range(players):
